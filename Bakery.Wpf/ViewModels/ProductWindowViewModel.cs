da85ebf1c2c113d19950a33ed849fb55c87141a4
Name <Name@DESKTOP-6ADNBQH>
2020-12-06 21:02:46 +0100

    commands

--- Bakery.Wpf/ViewModels/ProductWindowViewModel.cs
+++ Bakery.Wpf/ViewModels/ProductWindowViewModel.cs
@@ -13,11 +13,40 @@ namespace Bakery.Wpf.ViewModels
     {
         private Product _selectedProduct;
         private Product _temporaryProduct;
+        private string _productNr;
+        private string _name;
+        private double _price;
 
         public string Title { get; set; }
         public RelayCommand CmdSaveProduct { get; set; }
         public RelayCommand CmdUnload { get; set; }
-
+        public string ProductNr 
+        {
+            get => _productNr;
+            set
+            {
+                _productNr = value;
+                OnPropertyChanged(nameof(ProductNr));
+            } 
+        }
+        public string Name 
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        public double Price 
+        {
+            get => _price;
+            set
+            {
+                _price = value;
+                OnPropertyChanged(nameof(Price));
+            }
+        }
         public Product SelectedProduct
         {
             get => _selectedProduct;
@@ -42,11 +71,28 @@ namespace Bakery.Wpf.ViewModels
                 execute: async _ =>
                 {
                     await using IUnitOfWork unitOfWork = new UnitOfWork();
+                    Product product = new Product
+                    {
+                        ProductNr = ProductNr,
+                        Name = Name,
+                        Price = Price
+                    };
+                    SelectedProduct = product;
+                    
                     await unitOfWork.Products
                         .AddProductAsync(SelectedProduct);
+                    try
+                    {
+                        await unitOfWork.SaveChangesAsync();
+                    }
+                    catch (ValidationException e)
+                    {
+                        throw new ValidationException(e.Message);
+                    }
+
                 }
                 ,
-                canExecute: _ => SelectedProduct != null
+                canExecute: _ => ProductNr != null 
                 )
                 ;
             CmdUnload = new RelayCommand(
@@ -55,7 +101,7 @@ namespace Bakery.Wpf.ViewModels
                     SelectedProduct = _temporaryProduct;
                 }
                 ,
-                canExecute: _ => SelectedProduct != null
+                canExecute: _ => ProductNr != null
                 )
                 ;
         }
@@ -72,6 +118,9 @@ namespace Bakery.Wpf.ViewModels
             {
                 SelectedProduct = await unitOfWork.Products.GetProductByIdAsync(product.Id);
                 _temporaryProduct = SelectedProduct;
+                ProductNr = SelectedProduct.ProductNr;
+                Name = SelectedProduct.Name;
+                Price = SelectedProduct.Price;
                 Title = "Produkt bearbeiten";
             }
             else
